a906f69e6798dd4e8f5b4d287292fc33d8175c7e
arnjng <105932181+arnjng@users.noreply.github.com>
2022-08-25 15:18:27 +0200

    Improved the times table, slight changes in rest

--- main.js
+++ main.js
@@ -1,6 +1,115 @@
 var zeitenArray;
+var completedLaps = 0;
 
-document.body.onload = fillTable;
+var 
+
+document.body.onload = simulateNewLap;
+
+function simulateNewLap(){
+
+    if(completedLaps > 0){
+
+
+
+    }
+
+    completedLaps++;
+
+    fillArray();
+
+    for(var i=0; i<6; i++){
+
+        const row = document.createElement("tr");
+
+        const cellA = document.createElement("td");
+        cellA.textContent = (i+1).toString();
+        const pilotNo = i+1;
+        cellA.id = "pilot"+pilotNo.toString();
+        cellA.className = "pilotNumber";
+
+        const cellB = document.createElement("td");
+        cellB.appendChild(document.createTextNode(zeitenArray[i]));
+        cellB.id = "currentLap"+i;
+        cellB.className = "currentLap";
+
+        const cellC = document.createElement("td");
+        cellC.appendChild(document.createTextNode("N. A."));
+        cellC.id = "bestLap"+i;
+        cellC.className = "bestLap";
+
+        row.appendChild(cellA);
+        row.appendChild(cellB);
+        row.appendChild(cellC);
+
+        document.getElementById("tableLapTimes").appendChild(row);
+
+    }
+
+}
+
+function fillArray(){
+
+    zeitenArray = [generateTime(), generateTime(), generateTime(), generateTime(), generateTime(), generateTime()];
+
+}
+
+function quickSort(originalArr) {
+    if (originalArr.length <= 1) {
+       return originalArr;
+       } else {
+             var leftArr = [];              
+             var rightArr = [];
+             var newArr = [];
+             var pivot = originalArr.pop();      //  Take a pivot value
+             var length = originalArr.length;
+             for (var i = 0; i < length; i++) {
+                if (originalArr[i] <= pivot) {    // using pivot value start comparing
+                   leftArr.push(originalArr[i]);      
+             } else {
+                     rightArr.push(originalArr[i]);
+           }
+         }
+       return newArr.concat(quickSort(leftArr), pivot, quickSort(rightArr)); // array will be returned untill sorting occurs
+    }
+ }
+
+function timeConvert(time){
+
+    var timeMin = time/60;
+    var timeMin2 = Math.floor(timeMin);
+    
+    var TIME = timeMin2;
+
+
+    while(timeMin > 1){
+
+        timeMin--;
+
+    }
+    var timeSec = timeMin * 60;
+    var timeSec2 = Math.floor(timeSec);
+
+    var TIME = TIME + ":" + timeSec2;
+
+
+    while(timeSec > 1){
+
+        timeSec--;
+
+    }
+    var timeMiliSec = timeSec * 1000;
+    var timeMiliSec2 = Math.floor(timeMiliSec);
+
+    if(timeMiliSec2 < 100){
+
+        timeMiliSec2 = "0" + timeMiliSec2;
+
+    }
+
+
+    var TIME = TIME + ":" + timeMiliSec2;
+    return TIME;
+}
 
 function generateTime(){
 
@@ -15,78 +124,14 @@ function generateTime(){
     
     time = timeMulti * 87;
 
-    time2 = timeConvert(time);
+    var time2 = timeConvert(time);
+    time2 = time2.toString();
 
     return time2;
 
 }
 
-function fillArray(){
-
-    zeitenArray = [generateTime(), generateTime(), generateTime(), generateTime(), generateTime(), generateTime()];
-
-}
-
-function sortArray(){
-
-    
-
-}
-
-function fillTable(){
-
-    fillArray();
-
-    for(var i=0; i<6; i++){
-
-        const row = document.createElement("tr");
-        row.appendChild(document.createTextNode(zeitenArray[i]));
-
-        document.getElementById("times").appendChild(row);
-
-    }
-
-}
-
-function timeConvert(time){
-
-    var timeMin = time/60;
-    timeMin2 = Math.floor(timeMin);
-    
-    var TIME = timeMin2;
-
-
-    while(timeMin > 1){
-
-        timeMin--;
-
-    }
-    var timeSec = timeMin * 60;
-    timeSec2 = Math.floor(timeSec);
-
-    var TIME = TIME + ":" + timeSec2;
-
-
-    while(timeSec > 1){
-
-        timeSec--;
-
-    }
-    var timeMiliSec = timeSec * 1000;
-    timeMiliSec2 = Math.floor(timeMiliSec);
-
-    if(timeMiliSec2 < 100){
-
-        timeMiliSec2 = "0" + timeMiliSec2;
-
-    }
-
-
-    var TIME = TIME + ":" + timeMiliSec2;
-    return TIME;
-}
-
-function averageTime(){
+/*function averageTime(){
 
     var timeGlob = 0;
 
@@ -101,4 +146,4 @@ function averageTime(){
 
     document.getElementById("test").innerHTML = timeAv;
 
-}
+}*/
